f23998b80ce772bc187c220bb26cbb8801880fee
imsebastiand <42312495+imsebastiand@users.noreply.github.com>
2023-04-20 16:40:59 -0500

    update server

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, use_pages=True)
 
+server = app.server
 app.title = 'Matriz de Reporte'
 app.layout = html.Div([
     html.Div([
